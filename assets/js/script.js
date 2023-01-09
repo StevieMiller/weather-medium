619aa82383d492b2c3c360cec4699bc3445944ef
StevieMiller <stephanie.miller@carlsbadschools.net>
2023-01-09 09:28:10 -0700

    converted temp

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,9 +10,12 @@ let cityLat;
 let cityLon;
 
 
+
+
+
+
 // function calls the Open Weather API
 function getApi() {
-  console.log('clicked!');
 
   let cityChosen = inputCityEl.value
   let stateChosen = inputStateEl.value
@@ -22,39 +25,16 @@ function getApi() {
   var geocodeUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityChosen},${stateChosen},${countryChosen}&appid=${apiKey}`
 
   
-  console.log(geocodeUrl);
   
   fetch(geocodeUrl)
   .then((response) => response.json())
   .then((data) => {
-    console.log("Data:", data)
     cityName = data[0].name;
     cityLat = data[0].lat;
     cityLon = data[0].lon;
 
     // let forecastUrl gets the 5 day / 3 hour forecast data
-    let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLon}&appid=${apiKey}`
-    console.log(forecastUrl);
-    
-    
-
-    //getResponse(forecastUrl, cityName, cityLat, cityLon)
-    // fetch(forecastUrl)
-    // .then((res) => res.json())
-    // .then((forecastData) => console.log(forecastData));
-
-    // let cityInfo = {
-    //   country: '',
-    //   lat: '',
-    //   lon: '',
-    //   name: '',
-    //   state: ''
-    // }
-    // cityInfo[country] = data.country;
-    // cityInfo[lat] = data.lat;
-    // cityInfo[lon] = data.lon;
-    // cityInfo[name] = data.name;
-    // cityInfo[state] =  data.state;
+    let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLon}&units=imperial&appid=${apiKey}`
 
      getResponse(forecastUrl)
   });
@@ -63,8 +43,13 @@ function getApi() {
     
 }
 
+
+
+
+
+
+
 async function getResponse(forecastUrl) {
-  console.log(cityName)
 	const response = await fetch(
 		forecastUrl,
 		{
@@ -91,31 +76,43 @@ function get5Day(forecastData) {
         let oneDay = forecastData.list[i]
         let date = oneDay.dt
         let icon = oneDay.weather[0].icon
-        let tempMax = oneDay.main.temp_max
+        let temp = oneDay.main.temp
         let humidity = oneDay.main.humidity
         let windSpeed = oneDay.wind.speed
-        // Good luck!
         // Write HTML dynamically to append the data to the page
 
         
-        show5DayWeather(i, date, icon, tempMax, humidity, windSpeed)
-        showTodaysWeather(i, date, icon, tempMax, humidity, windSpeed)
+        show5DayWeather(i, date, icon, temp, humidity, windSpeed)
+        showTodaysWeather(i, date, icon, temp, humidity, windSpeed)
       }
 }
 
 
-function show5DayWeather(i, date, icon, tempMax, humidity, windSpeed) {
+
+
+
+
+
+
+function show5DayWeather(i, date, icon, temp, humidity, windSpeed) {
 // Get weather data and append to page 
-let dayCard = document.getElementById("day" + i) // if this doesn't work, make i a string
-dayCard.innerHTML = `Date: ${date}\nIcon: ${icon}\nTemp: ${tempMax}\nHumidity: ${humidity}\nWind: ${windSpeed}\n`
+let dayCard = document.getElementById("day" + i)
+dayCard.innerHTML = `Date: ${date}\nIcon: ${icon}\nTemp: ${temp}\nHumidity: ${humidity}\nWind: ${windSpeed}\n`
 console.log(dayCard);
 }
 
-function showTodaysWeather(i, date, icon, tempMax, humidity, windSpeed) {
+
+
+
+
+
+
+
+
+function showTodaysWeather(i, date, icon, temp, humidity, windSpeed) {
   // Get weather data and append to page
 
-let unixFormat = dayjs.unix(date).format('MMM D, YYYY');
-let iconUrl = "https://openweathermap.org/img/w" + icon + ".png";
+let unixFormat = dayjs.unix(date).format('MMM D, YYYY'); // Gets today's date
 
 let cityDisplay = document.getElementById("city")
 let dateDisplay = document.getElementById("date")
@@ -124,12 +121,12 @@ let tempDisplay = document.getElementById("temp")
 let humidityDisplay = document.getElementById("humidity")
 let windDisplay = document.getElementById("wind")
 
-cityDisplay.textContent = (cityName)
-dateDisplay.textContent = (unixFormat)
-iconDisplay.textContent = (icon)
-tempDisplay.textContent = ("Temp: " + tempMax)
-humidityDisplay.textContent = ("Humidity: " + humidity + "%")
-windDisplay.textContent = ("Wind Speed: " + windSpeed)
+cityDisplay.textContent = (cityName) // Displays city
+dateDisplay.textContent = (unixFormat) // Displays today's date
+iconDisplay.textContent = (icon) // Displays weather icon
+tempDisplay.textContent = ("Temp: " + temp) // Displays max temperature
+humidityDisplay.textContent = ("Humidity: " + humidity + "%") // Displays humidity
+windDisplay.textContent = ("Wind Speed: " + windSpeed) // Displays wind speed
 
 
 
